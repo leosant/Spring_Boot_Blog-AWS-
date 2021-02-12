1fe8b0121fdc98bf15c58dd2bb20200bade78af6
leosant <leonardosantos45548@gmail.com>
2021-02-11 23:22:06 -0300

    Downgrade of maven 2.1.9.RELEASE

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -3,10 +3,11 @@ package com.spring.codeblog.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,7 +45,7 @@ public class CodeblogController {
 	}
 	
 	@RequestMapping(value= "/newpost", method = RequestMethod.POST)
-	public String savePost(@Validated Post post, BindingResult result, RedirectAttributes attributes) {
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return "redirect:/newpost";
 		}
